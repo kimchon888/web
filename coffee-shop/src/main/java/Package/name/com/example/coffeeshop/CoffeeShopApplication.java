334aedd1e7352929f517ed49077b497fb5022fc1
phamn <anhvotinh500@gmail.com>
2024-11-23 23:35:43 +0700

    Initial commit

---
+++ coffee-shop/src/main/java/Package/name/com/example/coffeeshop/CoffeeShopApplication.java
@@ -0,0 +1,21 @@
+package Package.name.com.example.coffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@SpringBootApplication
+public class CoffeeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeShopApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
+
+
+}
---
+++ coffee-shop/src/main/java/Package/name/com/example/coffeeshop/services/ProductsRepository.java
@@ -0,0 +1,12 @@
+package Package.name.com.example.coffeeshop.services;
+
+
+import Package.name.com.example.coffeeshop.controllers.models.Product;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductsRepository extends JpaRepository<Product, Long> {
+    List<Product> findByIsHiddenFalse();
+}
